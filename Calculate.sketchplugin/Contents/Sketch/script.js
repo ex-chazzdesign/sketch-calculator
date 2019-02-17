aa9b7814401227b0d23d01a43bb9fd347be2e3e6
Diego Cano <diegocano@MacBook-Pro-de-Diego.local>
2019-02-17 20:42:32 +0100

    Fixes #1

--- Calculate.sketchplugin/Contents/Sketch/script.js
+++ Calculate.sketchplugin/Contents/Sketch/script.js
@@ -1,11 +1,30 @@
 var Sketch = require('sketch');
 var UI = require('sketch/ui')
+var Artboard = require('sketch/dom').Artboard
 
 var calculate = function(context) {
 
   var currentPage = Sketch.getSelectedDocument().selectedPage;
   var layers = currentPage.layers;
   for (var i=0; i < layers.length; i++) {
+    if (layers[i].type == 'Artboard') {      
+      processGroup(layers[i]);
+    } else {      
+      // Lo que no está en ningún artboard por ahora no lo procesamos
+      // console.log(layers[i].name+" no es un artboard");
+    }
+  }
+}
+
+var processGroup = function(group) {
+
+  var layers = group.layers;
+  for (var i=0; i < layers.length; i++) {
+
+    if (layers[i].type == 'Group'){
+      processGroup(layers[i]);      
+    }
+
     if (layers[i].name.startsWith("=")) {
       processLayer(layers[i]);
     }
@@ -13,11 +32,16 @@ var calculate = function(context) {
 }
 
 var processLayer = function(layer) {  
+
   var str = layer.name.substring(1, layer.name.length);  
+  var artboard = layer.getParentArtboard();
 
   while(str.includes("{")) {
     var token = str.substring(str.indexOf("{")+1, str.indexOf("}"));
+
+    // TO-DO: buscar solo en el mismo artboard, no en todo el documento
     var foundLayers = Sketch.getSelectedDocument().getLayersNamed(token);
+
     if (foundLayers.length == 0) {
       UI.message('No se ha encontrado '+token);
       layer.text = "ERR";
